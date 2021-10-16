8b6dadf1fe63ddfbc271d898ee037449a6daa391
TheZoq2 <frans.skarman@protonmail.com>
2021-10-16 11:07:49 +0200

    Improve reporting of unexpected tokens

--- spade-parser/src/error_reporting.rs
+++ spade-parser/src/error_reporting.rs
@@ -7,30 +7,43 @@ use codespan_reporting::files::SimpleFiles;
 use codespan_reporting::term::{self, termcolor::StandardStream};
 use spade_common::error_reporting::{codespan_config, color_choice, CompilationError};
 
+fn unexpected_token_list<'a>(expected: impl IntoIterator<Item = &'a str>) -> String {
+    let expected = expected
+        .into_iter()
+        .map(|s| format!("`{}`", s))
+        .collect::<Vec<_>>();
+
+    let count = expected.len();
+    if count == 1 {
+        format!("{}", expected[0])
+    } else {
+        format!(
+            "{}, or {}",
+            expected[0..(count - 1)].join(", "),
+            expected[count - 1]
+        )
+    }
+}
+
+fn unexpected_token_message<'a>(got: &Token, expected_list: &str) -> String {
+    format!(
+        "Unexpected `{}`, expected {}",
+        got.kind.as_str(),
+        expected_list,
+    )
+}
+
 fn unexpected_token<'a>(
     file_id: usize,
     got: Token,
     expected: impl IntoIterator<Item = &'a str>,
 ) -> Diagnostic<usize> {
-    let expected_list = format!(
-        "[{}]",
-        expected
-            .into_iter()
-            .map(|s| s.to_string())
-            .collect::<Vec<_>>()
-            .join(", ")
-    );
+    let expected_list = unexpected_token_list(expected);
+    let message = unexpected_token_message(&got, &expected_list);
 
-    let message = format!(
-        "Unexpected token. Got `{}`, expected one of {}",
-        got.kind.as_str(),
-        expected_list,
-    );
-
-    Diagnostic::error()
-        .with_message(message)
-        .with_labels(vec![Label::primary(file_id, got.span)
-            .with_message(format!("expected one of {}", expected_list))])
+    Diagnostic::error().with_message(message).with_labels(vec![
+        Label::primary(file_id, got.span).with_message(format!("expected {}", expected_list))
+    ])
 }
 
 impl CompilationError for Error {
@@ -52,10 +65,7 @@ impl CompilationError for Error {
                     Label::primary(file_id, friend.span).with_message(format!("to close this"))
                 ]),
             Error::ExpectedExpression { got } => {
-                let message = format!(
-                    "Unexpected token. Got {} expected expression",
-                    got.kind.as_str(),
-                );
+                let message = format!("Unexpected `{}`, expected expression", got.kind.as_str(),);
 
                 Diagnostic::error()
                     .with_message(message)
@@ -130,5 +140,32 @@ impl CompilationError for Error {
         let writer = StandardStream::stderr(color_choice(no_color));
 
         term::emit(&mut writer.lock(), &codespan_config(), &files, &diag).unwrap();
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn unexpected_token_works_for_single_token() {
+        let got = Token {
+            span: 0..0,
+            kind: crate::lexer::TokenKind::Assignment,
+        };
+        let expected_list = unexpected_token_list(vec!["=="]);
+        let result = unexpected_token_message(&got, &expected_list);
+        assert_eq!(result, "Unexpected `=`, expected `==`");
+    }
+
+    #[test]
+    fn unexpected_token_works_for_multiple_tokens() {
+        let got = Token {
+            span: 0..0,
+            kind: crate::lexer::TokenKind::Assignment,
+        };
+        let expected_list = unexpected_token_list(vec!["x", "y", "z"]);
+        let result = unexpected_token_message(&got, &expected_list);
+        assert_eq!(result, "Unexpected `=`, expected `x`, `y`, or `z`");
     }
 }
