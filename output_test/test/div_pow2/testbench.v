1484503e4d736226f1bcc7c209fea6db1d966d52
TheZoq2 <frans.skarman@protonmail.com>
2022-07-25 14:53:08 +0200

    Fix div_pow2

---
+++ output_test/test/div_pow2/testbench.v
@@ -0,0 +1,51 @@
+`include "vatch/main.v"
+
+module counter_tb();
+    `SETUP_TEST
+
+
+    reg[7:0] x;
+    reg[7:0] y;
+
+    wire[7:0] result;
+
+    integer x_;
+    integer y_;
+    integer div_result;
+    real div_result_real;
+
+    initial begin
+        for (x_ = -128; x_ < 127; x_ = x_ + 1) begin
+            for (y_ = 0; y_ < 8; y_ = y_ + 1) begin
+                div_result_real = $itor(x_) / 2.0**$itor(y_);
+                // We want to always round towards 0 here
+                if (div_result_real < 0 && $abs(div_result_real - $floor(div_result_real)) == 0.5) begin
+
+                    div_result = real'(div_result_real + 0.5);
+                end
+                else begin
+                    div_result = real'(div_result_real);
+                end
+
+                x = x_;
+                y = y_;
+
+                #1
+                // $display("%0d `div_pow2` %0d = %f (%0d). Got %0d", x_, 2.0 ** y_, div_result_real, div_result, $signed(result));
+
+
+                `ASSERT_EQ(result, $signed(8'(div_result)))
+            end
+        end
+
+        #20
+
+        `END_TEST
+    end
+
+    e_counter counter
+        ( .x_i(x)
+        , .y_i(y)
+        , .output__(result)
+        );
+endmodule
--- spade-mir/src/codegen.rs
+++ spade-mir/src/codegen.rs
@@ -178,14 +178,11 @@ fn statement_code(statement: &Statement, source_code: &CodeBundle) -> Code {
                     let divisor = &ops[1];
                     code! {
                         [0] "always_comb begin";
-                        [1]     format!("if ({dividend} == 0) begin");
+                        [1]     format!("if ({divisor} == 0) begin");
                         [2]         format!("{name} = {dividend};");
                         [1]     "end";
-                        [1]     format!("else if ($signed({divisor}) < 0) begin");
-                        [2]         format!("{name} = $signed({dividend} + (1 << ({divisor} - 1))) >>> {divisor};");
-                        [1]     "end";
-                        [1]     "else begin";
-                        [2]         format!("{name} = {dividend} >> {divisor};");
+                        [1]     format!("else begin");
+                        [2]         format!("{name} = $signed($signed({dividend}) + $signed(1 << ({divisor} - 1))) >>> $signed({divisor});");
                         [1]     "end";
                         [0] "end";
                     }.to_string()
@@ -1427,14 +1424,11 @@ mod expression_tests {
             r#"
             logic[2:0] _e_0;
             always_comb begin
-                if (_e_1 == 0) begin
+                if (_e_2 == 0) begin
                     _e_0 = _e_1;
                 end
-                else if ($signed(_e_2) < 0) begin
-                    _e_0 = $signed(_e_1 + (1 << (_e_2 - 1))) >>> _e_2;
-                end
                 else begin
-                    _e_0 = _e_1 >> _e_2;
+                    _e_0 = $signed($signed(_e_1) + $signed(1 << (_e_2 - 1))) >>> $signed(_e_2);
                 end
             end"#
         };
