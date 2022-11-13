10588ce809eb627ff5e87c952267c0b58d9abbe3
Gustav Sörnäs <gustav@sornas.net>
2022-11-13 16:55:29 +0100

    derive IntoDiagnostic

--- spade-ast-lowering/src/error.rs
+++ spade-ast-lowering/src/error.rs
@@ -1,5 +1,5 @@
 use spade_common::location_info::Loc;
-use spade_diagnostics::Diagnostic;
+use spade_macros::IntoDiagnostic;
 use thiserror::Error;
 
 pub enum ItemKind {
@@ -7,33 +7,22 @@ pub enum ItemKind {
     Entity,
 }
 
+#[derive(IntoDiagnostic)]
+#[diagnostic(error, "Can't create a wire of ports")]
 pub(crate) struct WireOfPort {
+    #[diagnostic(primary, "This can't be a wire")]
     pub(crate) full_type: Loc<()>,
+    #[diagnostic(secondary, "Because this is a port")]
     pub(crate) inner_type: Loc<()>,
 }
 
-impl From<WireOfPort> for Diagnostic {
-    fn from(err: WireOfPort) -> Self {
-        Diagnostic::error(err.full_type, "Can't create a wire of ports")
-            .primary_label("This can't be a wire")
-            .secondary_label(err.inner_type, "Because this is a port")
-    }
-}
-
+#[derive(IntoDiagnostic)]
+#[diagnostic(error, "Expected {} arguments, got {}", diag.expected, diag.got)]
 pub(crate) struct PatternListLengthMismatch {
     pub(crate) expected: usize,
     pub(crate) got: usize,
+    #[diagnostic(primary, "Expected {} arguments here", diag.expected)]
     pub(crate) at: Loc<()>,
-}
-
-impl From<PatternListLengthMismatch> for Diagnostic {
-    fn from(err: PatternListLengthMismatch) -> Self {
-        Diagnostic::error(
-            err.at,
-            format!("Expected {} arguments, got {}", err.expected, err.got),
-        )
-        .primary_label(format!("Expected {} arguments here", err.expected))
-    }
 }
 
 #[derive(Error, Debug, PartialEq, Clone)]
--- spade-macros/src/lib.rs
+++ spade-macros/src/lib.rs
@@ -1,6 +1,14 @@
 use proc_macro::TokenStream;
-use quote::ToTokens;
-use syn::{parse, parse_macro_input, parse_quote, ImplItemMethod};
+use proc_macro2::Literal;
+use proc_macro2::TokenStream as TokenStream2;
+use proc_macro_error::{abort, abort_call_site, proc_macro_error};
+use quote::{quote, ToTokens};
+use syn::{
+    parse::{self, Parse, ParseStream},
+    parse_macro_input, parse_quote,
+    punctuated::Punctuated,
+    Expr, Fields, Ident, ImplItemMethod, Token,
+};
 
 // Thanks to discord user Yandros(MemeOverloard) for doing the bulk of the work with this
 // macro
@@ -41,4 +49,127 @@ pub fn trace_typechecker(attrs: TokenStream, input: TokenStream) -> TokenStream 
         ret
     });
     input.into_token_stream().into()
+}
+
+enum DiagnosticMessage {
+    Literal(Literal),
+    Formatted(Literal, Punctuated<Expr, Token![,]>),
+}
+
+impl Parse for DiagnosticMessage {
+    fn parse(input: ParseStream) -> syn::Result<Self> {
+        let first = input.parse()?;
+        let _: Option<Token![,]> = input.parse()?;
+        if input.is_empty() {
+            return Ok(DiagnosticMessage::Literal(first));
+        }
+        let rest = Punctuated::parse_terminated(input)?;
+        Ok(DiagnosticMessage::Formatted(first, rest))
+    }
+}
+
+impl DiagnosticMessage {
+    fn quote(&self) -> TokenStream2 {
+        match self {
+            DiagnosticMessage::Literal(lit) => quote!( #lit ),
+            DiagnosticMessage::Formatted(lit, rest) => quote!(format!(#lit, #rest)),
+        }
+    }
+}
+
+struct DiagnosticAttribute {
+    ident: Ident,
+    message: Option<DiagnosticMessage>,
+}
+
+impl Parse for DiagnosticAttribute {
+    fn parse(input: ParseStream) -> syn::Result<Self> {
+        let ident = input.parse()?;
+        let _: Token![,] = input.parse()?;
+        let message = (!input.is_empty()).then(|| input.parse()).transpose()?;
+        Ok(DiagnosticAttribute { ident, message })
+    }
+}
+
+#[proc_macro_error]
+#[proc_macro_derive(IntoDiagnostic, attributes(diagnostic))]
+pub fn derive_diagnostic(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as syn::ItemStruct);
+    let fields = match &input.fields {
+        Fields::Named(fields) => fields,
+        Fields::Unnamed(_) => abort!(input.fields, "only on structs with named fields"),
+        Fields::Unit => abort!(input.fields, "only on structs with named fields"),
+    };
+    let ident = input.ident;
+    let top_attribute = input
+        .attrs
+        .iter()
+        .find(|attr| {
+            attr.path
+                .get_ident()
+                .map(|ident| ident == "diagnostic")
+                .unwrap_or(false)
+        })
+        .unwrap_or_else(|| abort_call_site!("missing outer #[diagnostic] attribute"));
+    let DiagnosticAttribute {
+        ident: level,
+        message: primary_message,
+    } = top_attribute.parse_args().unwrap();
+    let primary_message = primary_message.map(|msg| msg.quote());
+    let inner_attributes: Vec<(DiagnosticAttribute, &Ident)> = fields
+        .named
+        .iter()
+        .filter_map(|field| {
+            field.ident.as_ref().map(|ident| {
+                std::iter::zip(
+                    field.attrs.iter().filter(|attr| {
+                        attr.path
+                            .get_ident()
+                            .map(|ident| ident == "diagnostic")
+                            .unwrap_or(false)
+                    }),
+                    std::iter::repeat(ident),
+                )
+            })
+        })
+        .flatten()
+        .filter_map(|(attr, field)| attr.parse_args().ok().map(|attr| (attr, field)))
+        .collect();
+    let primary = inner_attributes
+        .iter()
+        .find(|(attr, _)| attr.ident == "primary")
+        .unwrap_or_else(|| abort_call_site!("primary span is required"));
+    let primary_span = primary.1;
+    let primary_label = primary
+        .0
+        .message
+        .as_ref()
+        .map(DiagnosticMessage::quote)
+        .map(|msg| quote!( .primary_label(#msg) ))
+        .unwrap_or_default();
+    let secondary_labels = inner_attributes
+        .iter()
+        .filter(|(attr, _)| attr.ident == "secondary")
+        .map(|(attr, field)| {
+            let message = attr
+                .message
+                .as_ref()
+                .map(DiagnosticMessage::quote)
+                .unwrap_or_else(|| abort_call_site!("secondary spans require a message"));
+            quote!( .secondary_label(diag.#field, #message) )
+        });
+
+    quote! {
+        impl std::convert::From<#ident> for ::spade_diagnostics::Diagnostic {
+            fn from(diag: #ident) -> Self {
+                ::spade_diagnostics::Diagnostic::#level(
+                    diag.#primary_span,
+                    #primary_message
+                )
+                #primary_label
+                #(#secondary_labels)*
+            }
+        }
+    }
+    .into()
 }
